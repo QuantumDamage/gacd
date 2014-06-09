a5183d5c3b3c62c191ff8483a2f1515f5c692990
Damian Melniczuk <damian.melniczuk@gmail.com>
2014-06-09 12:15:01 +0200

    dsf

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,14 @@
-rr
+# Reading three files of data from train data:
+train.set <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE, 
+                      sep = "")
+train.labels <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+train.subjects <- read.table("UCI HAR Dataset/train/subject_train.txt",
+                             header = FALSE)
+
+# Reading three files of data from test data:
+test.set <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE,
+                       sep = "")
+test.labels <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+test.subjects <- read.table("UCI HAR Dataset/test/subject_test.txt", 
+                            header = FALSE)
+
